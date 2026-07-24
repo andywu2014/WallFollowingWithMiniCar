87f6811bf9aca4fd9526a4b627ee1cd034803a85
andy <andywu2014@foxmail.com>
2026-07-24 12:52:38 +0800

    将逻辑写清晰

--- wallfollowing2.ts
+++ wallfollowing2.ts
@@ -12,47 +12,49 @@ namespace wallFollowing2 {
 			carState.history.setLatest(state)
 		}
 
-		let nowState = new carState.State()
-
 		while(true) {
+			let nowState = new carState.State()
 
-			// 没有干完的上一轮
-			basic.pause(800)
+			let drivingDurationMs = autoDrive(nowState)
+
+			basic.pause(drivingDurationMs)
 
 			nowState.leftDistance = sensor.readLeftDis()
 			carState.history.setLatest(nowState)
 
-			// next process  本轮
+		}
 
-			let state0 = carState.history.get(0)
+	}
 
-			if (state0.driving == carState.Driving.Stop) {
-				break
-			}
+	//
+	function autoDrive(nowState: carState.State): number {
+		let state0 = carState.history.get(0)
+		if (state0.driving == carState.Driving.Stop) {
+			// todo stop
+			return 0
+		}
 
-			let state_1 = carState.history.get(-1)
-			let diff = state_1.leftDistance - state0.leftDistance
+		let state_1 = carState.history.get(-1)
+		let diff = state_1.leftDistance - state0.leftDistance
 
-			if (state0.leftDistance < maze.LeftSensorExpectedDis - 5 && state0.driving == carState.Driving.GoingHead && diff > errLDiS) {
-				miniTank.TurnRight()
-				nowState.driving = carState.Driving.SlightRight
-				continue
-			}
+		if (state0.leftDistance < maze.LeftSensorExpectedDis - 5 && state0.driving == carState.Driving.GoingHead && diff > errLDiS) {
+			miniTank.TurnRight()
+			nowState.driving = carState.Driving.SlightRight
+			return 800
+		}
 
-			if (state0.leftDistance > maze.LeftSensorExpectedDis + 5 && state0.driving == carState.Driving.GoingHead && diff < errLDiS * -1) {
-				miniTank.TurnLeft()
-				nowState.driving = carState.Driving.SlightLeft
-				continue
-			}
+		if (state0.leftDistance > maze.LeftSensorExpectedDis + 5 && state0.driving == carState.Driving.GoingHead && diff < errLDiS * -1) {
+			miniTank.TurnLeft()
+			nowState.driving = carState.Driving.SlightLeft
+			return 800
+		}
 
-			if (state0.driving == carState.Driving.Prepare || state0.driving == carState.Driving.SlightLeft
-				|| state0.driving == carState.Driving.SlightRight) {
-				miniTank.Gohead()
-				nowState.driving = carState.Driving.GoingHead
-				continue
-			}
-
-
+		if (state0.driving == carState.Driving.Prepare || state0.driving == carState.Driving.SlightLeft
+			|| state0.driving == carState.Driving.SlightRight) {
+			miniTank.Gohead()
+			nowState.driving = carState.Driving.GoingHead
+			return 800
 		}
 	}
+
 }
