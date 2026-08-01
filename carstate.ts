7b6780e3510f63a597ef0f2ae84ca539c54fb802
andy <andywu2014@foxmail.com>
2026-08-01 19:08:18 +0800

    小写改大写

--- carstate.ts
+++ carstate.ts
@@ -22,7 +22,7 @@ namespace carState {
 	let end = false
 
 	export enum Driving {
-		Stop, Ready, GoingHead, SlightRight, SlightLeft, rightBack, leftBack, goingTargetDist
+		Stop, Ready, GoingHead, SlightRight, SlightLeft, RightBack, LeftBack, GoingTargetDist
 	}
 	const drivingStrs: string[] = ["Stop", "GoingHead", "SlightRight", "SlightLeft", "Ready"]
 	export function DrivingToStr(d: Driving): string {
@@ -35,6 +35,7 @@ namespace carState {
 		frontDistance: number
     angel: number
 		time: number
+		
 
 		constructor(){
 			this.driving = Driving.Stop
